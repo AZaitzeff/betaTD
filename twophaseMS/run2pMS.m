cd6a00b230cff8a1520c8eb63e667cb60a139335
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-02-20 21:53:17 -0500

    moretests

---
+++ twophaseMS/run2pMS.m
@@ -0,0 +1,29 @@
+function run2pMS(filename,fids,xranges,yranges)
+
+m=numel(xranges)-1;
+n=numel(yranges)-1;
+total=m*n;
+%parpool(5)
+%fids=[75,100,125,150,175];
+parfor section=1:total
+    for fid=fids
+        EBSDtemp=load(['../data/' filename 'alpha.mat']);
+        addpath('../anglelib/')
+        mapall=load(['../data/' filename 'ff.mat']);
+        factor=25;
+        i=mod(section-1,m)+1;
+        j=ceil(section/m);
+        rows=(max(xranges(i)-factor,1):min(xranges(i+1)+factor,xranges(end)));
+        cols=(max(yranges(j)-factor,1):min(yranges(j+1)+factor,yranges(end)));
+
+        EBSD=EBSDtemp.EBSD(rows,cols,:);
+        CI=EBSDtemp.CI(rows,cols);
+        map=mapall.map(rows,cols);
+        tic;
+        twophaseMS(EBSD,CI,map,fid,section);
+        toc;
+
+    end
+end
+
+puttogether(filename,fids,xranges,yranges)
