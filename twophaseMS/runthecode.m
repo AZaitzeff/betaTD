b1ff222fe9d8c3354f67339ab188a7acac7e91c9
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-04-11 12:04:41 -0400

    test2

--- twophaseMS/runthecode.m
+++ twophaseMS/runthecode.m
@@ -1,6 +1,11 @@
-filename='test1';
-run2pMS(filename,'beta0',[50,100,150,200,250,300],500,4,0);
-run2pMS(filename,'beta25',[50,100,150,200,250,300],500,4,.25);
-run2pMS(filename,'beta50',[50,100,150,200,250,300],500,4,.5);
-run2pMS(filename,'beta75',[50,100,150,200,250,300],500,4,.75);
-run2pMS(filename,'beta1',[50,100,150,200,250,300],500,4,1);
+%filename='test1';
+%run2pMS(filename,'beta0',[50,100,150,200,250,300],500,4,0);
+%run2pMS(filename,'beta25',[50,100,150,200,250,300],500,4,.25);
+%run2pMS(filename,'beta50',[50,100,150,200,250,300],500,4,.5);
+%run2pMS(filename,'beta75',[50,100,150,200,250,300],500,4,.75);
+
+
+
+filename='test2';
+run2pMS(filename,'beta50',[50,100,150,200],400,4,.5);
+run2pMS(filename,'beta1',[50,100,150,200],400,4,1);
