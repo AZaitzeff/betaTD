d8ead765049f15da400b981bf24e93abb97d27e9
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-05-10 21:20:20 -0400

    newtests

--- twophaseMS/runthecode.m
+++ twophaseMS/runthecode.m
@@ -1,14 +1,14 @@
-filename='test1';
+%filename='test1';
 %run2pMS(filename,'beta0',[50,100,150,200,250,300],500,4,0);
 %run2pMS(filename,'beta25',[50,100,150,200,250,300],500,4,.25);
 %run2pMS(filename,'beta50',[50,100,150,200,250,300],500,4,.5);
 %run2pMS(filename,'beta75',[50,100,150,200,250,300],500,4,.75);
-run2pMS(filename,'beta1',[250,500,750],500,4,1);
+%run2pMS(filename,'beta1',[250,500,750],500,4,1);
 
 
-filename='test2';
+%filename='test2';
 %run2pMS(filename,'betat250',[50,100,150,200],400,4,.5);
-run2pMS(filename,'betat21',[200],400,4,1);
+%run2pMS(filename,'betat21',[200],400,4,1);
 
 %filename='RX';
 %run2pMS(filename,'beta50RX',[100,150,200],400,4,.5);
