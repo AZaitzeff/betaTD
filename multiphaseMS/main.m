cec5aa45f96f8bf6fb58386f8f2ec1a961da6bae
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-10-04 09:44:39 -0400

    new

--- multiphaseMS/main.m
+++ multiphaseMS/main.m
@@ -1,10 +1,10 @@
 
 
-filename='AFone';
-runMStd(filename,'AFoneseg',[50,100,200],300,6,1,[6,8],2);
+%filename='AFone';
+%runMStd(filename,'AFoneseg',[50,100,200],300,6,1,[6,8],2);
 
 %filename='AFbig';
 %runMStd(filename,'AFbigseg',[50,100,200],400,6,1);
 
-%filename='sim';
-%runMStd(filename,'betatsim1',[50,100,200],300,4,1,[8,10],2);
+filename='sim';
+runMStd(filename,'betatsim1',[50,100,200],300,4,1,[8,10],2);
