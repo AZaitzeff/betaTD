bd60336eca94fd78ef24d638eede7a6b00bced78
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-10-05 16:06:49 -0400

    try again

--- multiphaseMS/main.m
+++ multiphaseMS/main.m
@@ -1,10 +1,10 @@
 
 
-%filename='AFone';
-%runMStd(filename,'AFoneseg',[200,300,400,500],600,4,1,[20,24],3,.02);
+filename='AFone';
+runMStd(filename,'AFoneseg',[200,300,400,500],600,2,1,[20,24],3,.02);
 
 %filename='AFbig';
 %runMStd(filename,'AFbigseg',[100,200,300,400],300,6,1,[8,10],2);
 
-filename='sim';
-runMStd(filename,'simseg',[200,300,400],600,1,1,[20,24],3,.02);
+%filename='sim';
+%runMStd(filename,'simseg',[200,300,400],600,1,1,[20,24],3,.02);
