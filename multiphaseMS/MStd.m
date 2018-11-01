cf6cc11e6ba1e2c8cda72251a162b1626c4f3a87
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-10-31 20:47:06 -0400

    now we are playing with power

--- multiphaseMS/MStd.m
+++ multiphaseMS/MStd.m
@@ -1,4 +1,4 @@
-function MStd(EBSD,CI,fid,Ks,filesave,dx,dy,dt,step,num,reestbeta,mexed)
+function MStd(EBSD,CI,fid,Ks,filesave,dx,dy,dt,step,num,mexed)
 
 if nargin<7
     dx=1/100;
@@ -14,31 +14,18 @@ if nargin<10
     num=1;
 end
 if nargin<11
-    reestbeta=0;
-end
-if nargin<11
-    reestbeta=0;
-end
-if nargin<12
     mexed=0;
 end
-[m,n,z]=size(EBSD);
+[m,n,~]=size(EBSD);
 
-
-enevec=zeros(1,num);
-maps=cell(1,num);
-dicts=cell(1,num);
-parfor i=1:num
 if step==1
     [mapall,newdict,newkappa]=initializeEBSD(EBSD,CI,Ks);
     [mapall,dict,~]=EBSDMStd(mapall,EBSD,CI,newdict,newkappa,fid,dx,dy,dt);
     energy=EBSDtdE(mapall,EBSD,CI,dict,fid,dx,dy);
-    enevec(i)=energy;
 elseif step<1
     [mapall,newdict,newkappa]=initializeEBSDfast(EBSD,CI,Ks,16,50,50,mexed);
     [mapall,dict,~]=EBSDMStd(mapall,EBSD,CI,newdict,newkappa,fid,dx,dy,dt,20,mexed);
     energy=EBSDtdEfast(mapall,EBSD,CI,dict,fid,dx,dy,mexed);
-    enevec(i)=energy;
     
 else
     sEBSD=EBSD(1:step:end,1:step:end,:);
@@ -49,30 +36,6 @@ else
     dt2=dt/(2*step);
     [mapall,dict,~]=EBSDMStdfast(mapall,EBSD,CI,dict,kappa,fid,dx,dy,dt2,0,mexed);
     energy=EBSDtdEfast(mapall,EBSD,CI,dict,fid,dx,dy,mexed);
-    enevec(i)=energy;
 end
-maps{i}=mapall;
-dicts{i}=dict;
+save(['results/' filesave num2str(fid) num2str(num)],'mapall','dict','energy');
 end
-[~,I]=min(enevec);
-
-mapall=maps{I};
-dict=dicts{I};
-if reestbeta
-    [dict]=estimatebetas(EBSD,CI,zeros(size(CI)),mapall,[],0,1,500);
-end
-betaEBSD=zeros(m*n,4);
-[clusterlist,~,~] = unique(mapall);
-alpha=reshape(EBSD, [m*n,z]);
-alpha=E313toq(alpha);
-for z=clusterlist'
-    mu=dict(z);
-    zfun=find(z==mapall);
-
-    [best,~]=alpbbeta(alpha(zfun,:),mu);
-    betaEBSD(zfun,:)=best;
-
-end
-betaEBSD=qtoE313(betaEBSD);
-betaEBSD=reshape(betaEBSD,[m,n,3]);
-save(['results/' filesave num2str(fid)],'mapall','betaEBSD','dict');
--- multiphaseMS/runMStd.m
+++ multiphaseMS/runMStd.m
@@ -26,11 +26,40 @@ EBSDtemp=load(['../data/' filename 'EBSD.mat']);
 addpath('../anglelib/')
 EBSD=EBSDtemp.EBSD;
 CI=EBSDtemp.CI;
+[m,n,z]=size(EBSD);
 %betas=EBSDtemp.betas(rows,cols);
 reestbeta=0;
 tic;
-MStd(EBSD,CI,fid,Ks,filesave,dx,dy,dt,step,num,reestbeta,mexed);
+parfor i=1:num
+MStd(EBSD,CI,fid,Ks,filesave,dx,dy,dt,step,i,reestbeta,mexed);
+end
 toc;
 
+enevec=zeros(1,num);
+for i=1:num
+    vars=load(['results/' filesave num2str(fid) num2str(I)]);
+    enevec(i)=vars.energy;
+end
+[~,I]=min(enevec);
+vars=load(['results/' filesave num2str(fid) num2str(I)]);
+mapall=vars.mapall;
+dict=vars.dict;
+if reestbeta
+    [dict]=estimatebetas(EBSD,CI,zeros(size(CI)),mapall,[],0,1,500);
+end
+betaEBSD=zeros(m*n,4);
+[clusterlist,~,~] = unique(mapall);
+alpha=reshape(EBSD, [m*n,z]);
+alpha=E313toq(alpha);
+for z=clusterlist'
+    mu=dict(z);
+    zfun=find(z==mapall);
+    [best,~]=alpbbeta(alpha(zfun,:),mu);
+    betaEBSD(zfun,:)=best;
+
+end
+betaEBSD=qtoE313(betaEBSD);
+betaEBSD=reshape(betaEBSD,[m,n,3]);
+save(['results/' filesave num2str(fid)],'mapall','betaEBSD','dict');
 poolobj = gcp('nocreate');
 delete(poolobj);
