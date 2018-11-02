efe8bb54c399866050f4e0956d8d7c044782ac30
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-11-01 20:58:46 -0400

    now we are playing with power

--- multiphaseMS/main.m
+++ multiphaseMS/main.m
@@ -1,9 +1,8 @@
-filename='AFone';
-%runMStd(filename,'AFoneFT',300,6,1,[11,7],1/100,1/100,.08,1,6,0,1);
-runMStd(filename,'AFoneCT',300,12,1,[11,7],1/100,1/100,.08,4,12,0,1);
-%Ks={[7,7],[7,7],[8,8],[8,8],[6,6],[6,6]};
-%filename='AFbig';
-%runMStd(filename,'AFbigCT',300,6,1,[12,7],1/100,1/100,.08,8,0,6);
-%Ks={[7,7],[7,7],[8,8],[8,8],[6,6],[6,6]};
+%filename='AFone';
+%runMStd(filename,'AFoneCT',300,12,1,[11,7],1/100,1/100,.08,4,12,0,1);
+
+filename='AFbig';
+runMStd(filename,'AFbigCT',300,12,1,[12,7],1/100,1/100,.16,8,12,0,1);
+
 %filename='sim';
 %runMStd(filename,'simseg',[50,100,150,200],1000,6,1,Ks,.02);
