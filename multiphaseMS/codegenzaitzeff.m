40e5554d78732d9b6507917f8de4962bc037ddd4
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-11-02 13:40:30 -0400

    now we are playing with power

--- multiphaseMS/codegenzaitzeff.m
+++ multiphaseMS/codegenzaitzeff.m
@@ -1,11 +1,11 @@
 %%
 
-vectorType = coder.typeof(1==1, [1000 1000], [true true]);
+vectorType = coder.typeof(1==1, [2000 2000], [true true]);
 codegen makerowcolmapsz -args {vectorType,(7),(7),(7),(7)}
 
 %%
 
-vectorType1 = coder.typeof(1, [1000 1000], [true true]);
+vectorType1 = coder.typeof(1, [2000 2000], [true true]);
 vectorType2 = coder.typeof(1, [2000 401], [false false]);
 vectorType3 = coder.typeof(1, [2000 1], [false false]);
 codegen ADIz -args {vectorType1,1,1,1,vectorType2,vectorType2,vectorType3,vectorType3,7,7}
