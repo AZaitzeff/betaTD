dbfd1c9bd811cf57f9b4444e66ce59d8f189a721
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-11-02 09:48:20 -0400

    now we are playing with power

--- multiphaseMS/main.m
+++ multiphaseMS/main.m
@@ -1,20 +1,20 @@
- for dt=[.08,.16]
-     for fid=[250,300,350]
-         name=['AFone' num2str(dt*100) 'dt'];
-         filename='AFone';
-         runMStd(filename,name,fid,12,1,[11,7],1/100,1/100,dt,4,24,1,1);
-     end
-end
+%  for dt=[.08,.16]
+%      for fid=[250,300,350]
+%          name=['AFone' num2str(dt*100) 'dt'];
+%          filename='AFone';
+%          runMStd(filename,name,fid,12,1,[11,7],1/100,1/100,dt,4,24,1,1);
+%      end
+% end
 
-% for step=[4,8,16]
-% for dt=[.08,.16,.32]
-%     for fid=[250,300,350]
-%         name=['AFbig' num2str(dt*100) 'dt' num2str(step) 'step'];
-%         filename='AFbig';
-%         runMStd(filename,name,fid,12,1,[12,7],1/100,1/100,dt,step,12,1,0);
-%     end
-% end
-% end
+for step=[4,8]
+for dt=[.08,.16]
+    for fid=[100,150,200]
+        name=['AFbig' num2str(dt*100) 'dt' num2str(step) 'step'];
+        filename='AFbig';
+        runMStd(filename,name,fid,12,1,[12,7],1/100,1/100,dt,step,16,1,1);
+    end
+end
+end
 
 %for dt=[.08,.16]
 %    for fid=[50,100,150]
