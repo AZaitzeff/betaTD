026da1226904825dba2c975a45f5dc8890ec84b5
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-11-26 20:32:27 -0500

    more

---
+++ multiphaseMS/readshockley.m
@@ -0,0 +1,10 @@
+function E=readshockley(b1,b2)
+
+theta=b2bmetric(b1,b2)*2;
+if theta<(1/180*pi)
+ E=2*pi*1/180*pi*(1-log(2*pi*1/180*pi));
+elseif theta<1/(2*pi)
+ E=2*pi*theta*(1-log(2*pi*theta));
+else
+ E=1;
+end
