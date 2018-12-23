338952038e48ed6cc0af2b1c311675bf8a8524c2
Alexander Zaitzeff <azaitzeff@gmail.com>
2018-12-22 19:31:48 -0500

    here

---
+++ multiphaseMS/converttobetamap.m
@@ -0,0 +1,22 @@
+function betaEBSD=converttobetamap(EBSD,beta,dict,mapall)
+[M,N]=size(mapall);
+betaEBSD=zeros(M*N,4);
+K = size(dict,1);
+alpha=reshape(EBSD, [M*N,3]);
+alpha=E313toq(alpha);
+
+[~,~,~,coords,sizecoords,~]=  bndcoords(mapall,K);
+for k=1:K
+    mu=dict(k,:);
+    indices=coords(k,1:sizecoords(k));
+    mask=beta(indices);
+    acoord=indices(~mask);
+    [best,~]=alpbbeta(alpha(acoord,:),mu);
+    betaEBSD(acoord,:)=best;
+    bcoord=indices(mask);
+    betaEBSD(bcoord,:)=alpha(bcoord,:);
+
+    
+end
+betaEBSD=qtoE313(betaEBSD);
+betaEBSD=reshape(betaEBSD,[M,N,3]);
