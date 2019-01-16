a6d69a33ed0a9227236632c6d0a751bff6a989d8
Alexander Zaitzeff <azaitzeff@gmail.com>
2019-01-15 21:43:54 -0500

    done?

--- anglelib/b2bmetric.m
+++ anglelib/b2bmetric.m
@@ -5,8 +5,9 @@ function dist=b2bmetric(beta1,beta2)
     dist=1.5*ones(1,n);
     
     for j=1:numS
-        beta1
-        beta2
+        
+        
         dist(:,:)=min(dist(:,:),acos(abs(beta2*(Pm(:,:,j)'*beta1'))));
     end
+    
 end
