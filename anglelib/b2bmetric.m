b41fbbb3868ec00eee9f0eb1f37bd6ff46c847e2
Alexander Zaitzeff <azaitzeff@gmail.com>
2019-01-16 09:18:44 -0500

    done?

--- anglelib/b2bmetric.m
+++ anglelib/b2bmetric.m
@@ -1,12 +1,11 @@
 function dist=b2bmetric(beta1,beta2)
-    Pm=real(getsymmetries('cubic'));
+    Pm=getsymmetries('cubic');
     numS=size(Pm,3);
     n=size(beta1,1);
     dist=1.5*ones(1,n);
-    beta2=real(beta2);
-    beta1=real(beta1);
+
     for j=1:numS
-        dist(:,:)=min(dist(:,:),acos(beta2*(Pm(:,:,j)'*beta1')));
+        dist(:,:)=min(dist(:,:),acos(abs((beta2*Pm(:,:,j)')*beta1')));
     end
     
 end
