d65fcfd3a448f23f5416460dccd803e83befeb26
Alexander Zaitzeff <azaitzeff@gmail.com>
2019-02-01 10:40:56 -0500

    I am here

--- multiphaseMS/main.m
+++ multiphaseMS/main.m
@@ -17,10 +17,10 @@
 %  end
  
 names={'AFbig','R1','R2','AFone','RX','rand','sim','AFbeta','AF225','AFpancake'};
-for i=[4:10 1]
+for i=[2:10 1]
   filename=names{i};
   name=names{i};
-  runMStd(filename,name,20,24,5);
+  runMStd(filename,name,20,30,6);
 end
 
 %names={'sim','sim2f','sim3f','sim4f'};
