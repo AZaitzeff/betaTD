de814a9f6db6cb4332b457108648f90f92a470da
Alexander Zaitzeff <azaitzeff@gmail.com>
2019-02-22 09:19:09 -0500

    long

--- multiphaseMS/main.m
+++ multiphaseMS/main.m
@@ -43,12 +43,12 @@ names={'AFbig','R1','R2','AFone','RX','rand','sim','AFbeta','AF225','AFpancake',
 %      runMStdsimple(filename,name,16,30,2^-4,fid,30,0);
 % end
 % % 
-% i=7;
-% for fid=[50]
-%      filename=names{i};
-%      name=names{i};
-%      runMStdsimple(filename,name,16,30,2^-4,fid,30,0);
-% end
+i=7;
+for fid=[50]
+     filename=names{i};
+     name=names{i};
+     runMStdsimple(filename,name,16,30,2^-4,fid,30,0);
+end
 
 
 % i=9;
@@ -59,12 +59,12 @@ names={'AFbig','R1','R2','AFone','RX','rand','sim','AFbeta','AF225','AFpancake',
 % end
 
 
-i=9;
-for fid=[100,150,200]
-     filename=names{i};
-     name=names{i};
-     runMStdsimple(filename,name,8,16,2^-4,fid,40,1);
-end
+% i=9;
+% for fid=[100,150,200]
+%      filename=names{i};
+%      name=names{i};
+%      runMStdsimple(filename,name,8,16,2^-4,fid,40,1);
+% end
 
 % 
 % i=10;
