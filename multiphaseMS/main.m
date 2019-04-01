f66b6fe7bb68a6568d69f0c4bde0bc735852ab06
Alexander Zaitzeff <azaitzeff@gmail.com>
2019-04-01 14:43:41 -0400

    add conf

--- multiphaseMS/main.m
+++ multiphaseMS/main.m
@@ -6,20 +6,20 @@ names={'AFbig','R1','R2','AFone','RX','rand','sim','AFbeta','AF225','AFpancake',
 %      name=[names{i} '8gs5dt'];
 %      runMStdsimple(filename,name,8,16,2^-5,fid,8,200,0);
 % end
-try
-z=1;
-for i=1:2
-    for j=1:3
-        for fid=[100,200]
-             filename=[names{z} 'row' num2str(i) 'col' num2str(j)];
-             name=[filename '8gs5dt'];
-             runMStdsimple(filename,name,8,16,2^-5,fid,8,200,0);
-        end
-    end
-end
-catch ME
-    ME.identifier
-end
+% try
+% z=1;
+% for i=1:2
+%     for j=1:3
+%         for fid=[100,200]
+%              filename=[names{z} 'row' num2str(i) 'col' num2str(j)];
+%              name=[filename '8gs5dt'];
+%              runMStdsimple(filename,name,8,16,2^-5,fid,8,200,0);
+%         end
+%     end
+% end
+% catch ME
+%     ME.identifier
+% end
 % 
 % for fid=[200,400,600]
 %      filename=names{i};
@@ -86,14 +86,21 @@ end
 %      runMStdsimple(filename,name,4,16,2^-4,fid,40,400,0);
 % end
 
+try
+z=9;
+for i=1:1
+    for j=1:4
 
-% i=9;
-% for fid=[100,150,200]
-%      filename=names{i};
-%      name=names{i};
-%      runMStdsimple(filename,name,8,16,2^-4,fid,40,1);
-% end
-
+     for fid=[100,150,200]
+          filename=[names{z} 'row' num2str(i) 'col' num2str(j)];
+          name=filename;
+          runMStdsimple(filename,name,8,16,2^-4,fid,30,1);
+     end
+    end
+end
+catch ME
+    ME.identifier
+end
 % 
 % i=10;
 % for fid=[100,150]
